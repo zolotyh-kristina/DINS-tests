3c2f326247ddc758a714425e4d095ddb4358acf2
kristina.zolotyh <ovxristina@gmail.com>
2021-04-29 00:26:00 +0300

    Добавила проверку на posts?userId=1

--- tests.js
+++ tests.js
@@ -46,6 +46,19 @@ testCase('/GET posts', () => {
     });
 });
 
+
+testCase('/GET posts?userId=1', () => {
+    it('it should GET the post with id=1', (done) => {
+      chai.request('https://jsonplaceholder.typicode.com')
+          .get('/posts?userId=1')
+          .end((err, res) => {
+              res.should.have.status(200);
+              //do not output all posts but please make output of few first posts of the received array
+              res.body.should.be.a('array');
+              done();
+          });
+    });
+});
 //tests for GET	/posts/:id - please create several tests (7 tests or more)
 
 //tests for POST	/posts - please create few tests (5 tests or more)
